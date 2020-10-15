ef708c8045dc935d0063ba35a6afcdfc5623f50f
jvesteves <joaovestevesp@gmail.com>
2020-10-15 20:08:47 -0300

    Dia 4 - BackEnd

---
+++ src/database/fakedata.js
@@ -0,0 +1,33 @@
+module.exports = [
+  {
+    id: 1,
+    lat: "-27.222633",
+    lng: "-49.6455874",
+    name: "Lar das meninas",
+    description: "Presta assistência a crianças de 06 a 15 anos que se encontre em situação de risco e/ou vulnerabilidade social.",
+    images: [
+      "https://images.unsplash.com/photo-1602724797380-57978874df4f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80",
+      "https://images.unsplash.com/photo-1602744352504-04eddc408637?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=635&q=80"
+    ],
+    instructions: "Venha como se sentir a vontade e traga muito amor e paciência para dar.",
+    opening_hours: " Horário de visitas Das 18h até 8h.",
+    open_on_weekends: "1"
+
+
+  },
+
+  {
+    id: 2,
+    lat: "-27.222633",
+    lng: "-49.6555874",
+    name: "Lar dos meninos",
+    description: "Presta assistência a crianças de 06 a 15 anos que se encontre em situação de risco e/ou vulnerabilidade social.",
+    images: [
+      "https://images.unsplash.com/photo-1602724797380-57978874df4f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80",
+      "https://images.unsplash.com/photo-1602744352504-04eddc408637?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=635&q=80"
+    ],
+    instructions: "Venha como se sentir a vontade e traga muito amor e paciência para dar.",
+    opening_hours: " Horário de visitas Das 18h até 8h.",
+    open_on_weekends: "0"
+  }
+]
---
+++ src/server.js
@@ -0,0 +1,21 @@
+// import lib
+
+const express = require("express");
+const path = require("path");
+const pages = require('./pages.js')
+//starting express
+const server = express()
+server
+.use(express.static("public"))
+
+//config template engine
+.set ('views', path.join(__dirname, "views"))
+.set ('view engine', 'hbs')
+
+// create an route
+.get("/", pages.index)
+.get("/orphanage", pages.orphanage)
+.get("/orphanages", pages.orphanages)
+.get("/create-orphanage", pages.createOrphanage)
+// server on
+server.listen(5500)
